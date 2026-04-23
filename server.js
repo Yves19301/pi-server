40fec20433fb4f31a3cc3abad34ec4b6bb3bccf4
Yves19301 <nzeyvesi@gmail.com>
2026-04-24 01:38:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+  console.log("Request received");
   res.send("Server is running");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
