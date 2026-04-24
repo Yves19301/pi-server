51f6e9669082fbe48c7eb66197d133afa5e939c0
Yves19301 <nzeyvesi@gmail.com>
2026-04-24 02:10:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  console.log("Request received");
   res.send("Server is running");
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+// APPROVE
+app.post("/approve", (req, res) => {
+  console.log("Approve:", req.body);
+  res.json({ success: true });
+});
+
+// COMPLETE
+app.post("/complete", (req, res) => {
+  console.log("Complete:", req.body);
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
